8bfe59f632fe4972747d03eaa5ca4f0690d230dd
René Pastoors <rene.pastoors@hsrw.org>
2020-10-28 17:14:24 +0100

    Erstes Prototyp Konzept
    - Button zum öffnen erstellt
    - Fenster für den Chatbot erstellt
      - kann wieder geschlossen
      - Nachrichten können eingegeben werden
        - jedoch noch nicht abgeschickt werden
      - Chatverlauf ist noch nicht implementiert

---
+++ Prototyp/RP/Chatbot.js
@@ -0,0 +1,7 @@
+function openChatbot() {
+    document.getElementById("Chatbot").style.transform = "scale(1)";
+}
+
+function closeChatbot() {
+    document.getElementById("Chatbot").style.transform = "scale(0)";
+}
