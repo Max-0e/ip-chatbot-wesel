6825be39cdf50aa152bcbde17fc9ceefc3f56db0
René Pastoors <rene.pastoors@hsrw.org>
2021-01-13 23:13:25 +0100

    einige optische anpassungen

--- Prototyp/gemeinsamerPrototyp/Chatbot.js
+++ Prototyp/gemeinsamerPrototyp/Chatbot.js
@@ -1,15 +1,20 @@
-Antworten = ["Hallo mein Name ist Chatbot, wie kann ich Ihnen weiterhelfen?", "Weitere Informationen finden Sie in unserem FAQ.", "Bitte wiederholen Sie die Frage.", 
-             "Der Kreis Wesel ist immer für Sie da."]
+var BlockBot = false;
 
 function send() {
     var Text = document.getElementById("ChatbotSendText").value;
-    if (Text == "") return;
+    if (Text == "" || BlockBot) return;
+    BlockBot = true;
     document.getElementById("ChatbotSendText").value = "";
     var NewMessage = document.createElement("DIV");
     NewMessage.classList.add("ChatbotMessage", "UserMessage");
     NewMessage.innerHTML = Text;
     document.getElementById("ChatbotContent").appendChild(NewMessage);
-    NewMessage.scrollIntoView();
+
+    var BotMessage = document.createElement("DIV");
+    BotMessage.classList.add("ChatbotMessage", "BotMessage");
+    BotMessage.append(getWaitPoints());
+    document.getElementById("ChatbotContent").appendChild(BotMessage);
+    BotMessage.scrollIntoView();            
     $.ajax({
         url: "http://localhost:5005/webhooks/rest/webhook",
         type: "POST",
@@ -17,29 +22,45 @@ function send() {
         dataType: "json",
         data: JSON.stringify({"sender":"test_user",  "message":Text}),
         success: function(result) {
+            let tmpNode = document.getElementById("ChatbotContent").lastChild;
+            tmpNode.removeChild(BotMessage.lastChild);
+            console.log(result);
+            let tmpArray = [];
             result.forEach(element => {
-                if (element.text !== "undefined")
-                    antwortBot(element.text);
+                for (let key in element) {
+                    if(key == "text")
+                        tmpArray.push(element[key]);
+                    else if (key == "image")
+                        tmpArray.push("<img style='width:100%' src='" + element[key] + "'/>"); 
+                }
             });
+            tmpNode.innerHTML = tmpArray.join("<br>");
+            if (tmpNode.innerHTML == "")
+                tmpNode.innerHTML = "Leider konnte ich nichts finden...";
+            BlockBot = false;       
+            tmpNode.scrollIntoView();    
         },
         error: function() {
-            antwortBot("Ich kann Ihnen leider nicht weiter helfen. (Error 404 Bot not Found)")
+            let tmpNode = document.getElementById("ChatbotContent").lastChild;
+            tmpNode.removeChild(BotMessage.lastChild);
+            tmpNode.innerHTML = "Ich kann Ihnen leider aktuell nicht weiter helfen.";
+            tmpNode.scrollIntoView();     
+            BlockBot = false;                  
         }
     });
 }
 
-function antwortBot(Antwort) {
-    var NewMessage = document.createElement("DIV");
-    NewMessage.classList.add("ChatbotMessage", "BotMessage");
-    NewMessage.append(getWaitPoints());
-    document.getElementById("ChatbotContent").appendChild(NewMessage);
-    NewMessage.scrollIntoView();            
+function antwortBot(Antwort) { //Funktion für statische Antworten
+    var BotMessage = document.createElement("DIV");
+    BotMessage.classList.add("ChatbotMessage", "BotMessage");
+    BotMessage.append(getWaitPoints());
+    document.getElementById("ChatbotContent").appendChild(BotMessage);
+    BotMessage.scrollIntoView();            
     setTimeout(function() {
-        NewMessage.removeChild(NewMessage.lastChild);
-        NewMessage.innerHTML = Antwort;
-        NewMessage.scrollIntoView();          
+        BotMessage.removeChild(BotMessage.lastChild);
+        BotMessage.innerHTML = Antwort;
+        BotMessage.scrollIntoView();                      
     }, 1500);
-
 }
 
 function getWaitPoints() {
