cb0ec86c2a74702fb119c5088b255b0b57c7f326
René Pastoors <rene.pastoors@hsrw.org>
2021-02-06 13:19:02 +0100

    uuid für sender meine Kerle

--- Prototyp/gemeinsamerPrototyp/Chatbot.js
+++ Prototyp/gemeinsamerPrototyp/Chatbot.js
@@ -1,5 +1,15 @@
 var BlockBot = false;
 var FirstOpen = true;
+var senderID;
+const URL = "http://localhost:5005/webhooks/rest/webhook";
+
+
+function uuid() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );  
+}
+
 function send() {
     var Text = document.getElementById("ChatbotSendText").value;
     if (Text == "" || BlockBot) return;
@@ -16,11 +26,11 @@ function send() {
     document.getElementById("ChatbotContent").appendChild(BotMessage);
     BotMessage.scrollIntoView();            
     $.ajax({
-        url: "http://localhost:5005/webhooks/rest/webhook",
+        url: URL,
         type: "POST",
         contentType: "application/json",
         dataType: "json",
-        data: JSON.stringify({"sender":"test_user",  "message":Text}),
+        data: JSON.stringify({"sender":senderID,  "message":Text}),
         success: function(result) {
             let tmpNode = document.getElementById("ChatbotContent").lastChild;
             tmpNode.removeChild(BotMessage.lastChild);
