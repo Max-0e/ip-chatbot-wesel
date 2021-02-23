832e479dde37c5a2e415cf1d3d9998a682c70209
25208 <maximilian.oedinger@hsrw.org>
2021-02-24 00:49:27 +0100

    mongodb anbindung angepasst

--- actions/testDBQuery.py
+++ actions/testDBQuery.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # mit MongoDB aus standard Port laufend verbinden
-client = MongoClient('mongodb://ip-chatbot.dnsuser.de:27017/')
+client = MongoClient('mongodb://ip-chatbot-wesel.dnsuser.de:27017/')
 # mit DB 'rasaBot' verbinden
 db = client.rasaBot
 # mit collection 'dienstleistungen' verbinden
